571b97ba5b14bc7f595ffe818d59ba894b71643e
Eugene Kataev <cataev.evgeni@yandex.ru>
2020-09-23 10:09:19 +0300

    [ESBL-90] Geocoder Update

--- src/Service/GeoCodeService.php
+++ src/Service/GeoCodeService.php
@@ -19,16 +19,16 @@ use GuzzleHttp\Client;
 
 class GeoCodeService
 {
-    private Here $hereProvider;
+    /*private Here $hereProvider;
     private GoogleMaps $googleMapsProvider;
     private Chain $chainProvider;
     private ProviderAggregator $providerAgregator;
     private GeocodeQuery $geocodeQuery;
     private ReverseQuery $reverseQuery;
-    private Client $guzzleClient;
+    private Client $guzzleClient;*/
     protected static $geocoder;
 
-    public function __construct(ProviderAggregator $providerAggregator, GeocodeQuery $geocodeQuery, ReverseQuery $reverseQuery, Chain $chain, Here $here, GoogleMaps $googleMaps, Client $client)
+    /*public function __construct(ProviderAggregator $providerAggregator, GeocodeQuery $geocodeQuery, ReverseQuery $reverseQuery, Chain $chain, Here $here, GoogleMaps $googleMaps, Client $client)
     {
         $this->providerAgregator = $providerAggregator;
         $this->geocodeQuery = $geocodeQuery;
@@ -37,13 +37,46 @@ class GeoCodeService
         $this->hereProvider = $here;
         $this->googleMapsProvider = $googleMaps;
         $this->guzzleClient = $client;
+    }*/
+
+    static protected function getGeoCoder()
+    {
+        if ( !self::$geocoder ) {
+            self::$geocoder = new ProviderAggregator();
+            $adapter = new Client();
+            $chain = new Chain([
+                new Here($adapter, 'eoQm7snFfZ4TZOd2gnPT', 'vJcc4TntD70lhe31n-HMpQ'),
+                new GoogleMaps($adapter, null, 'AIzaSyC983NJ9_Ub2jFbQ6mwdqlT8tGVjnmxhJQ')
+            ]);
+            self::$geocoder->registerProvider($chain);
+        }
+        return self::$geocoder;
     }
 
-    private function getGeoCoder()
+    public static function getLatLong(string $address)
     {
-        if (!self::$geocoder){
-            self::$geocoder = $this->providerAgregator;
-            $chain = $this->chainProvider;
+        $result = self::getGeoCoder()->geocodeQuery(GeocodeQuery::create($address));
+        if ( !$result->isEmpty() ) {
+            return [ 'lat' => $result->first()->getCoordinates()->getLatitude(), 'lng' => $result->first()->getCoordinates()->getLongitude() ];
         }
+        return null;
     }
+
+    public static function getAddress(string $lat, string $lng)
+    {
+        $result = self::getGeoCoder()->reverseQuery(ReverseQuery::fromCoordinates($lat, $lng));
+        if ( !$result->isEmpty() ) {
+            $i = $index = 0;
+            foreach ( $result->all() as $arItem ) {
+                if ( $arItem->getStreetName() != null && $arItem->getLocality() != null && $arItem->getPostalCode() != null && $arItem->getAdditionalData()['StateName'] != null ) {
+                    $index = $i;
+                    break;
+                }
+                $i++;
+            }
+            return [ 'street' => $result->get($index)->getStreetName(), 'city' => $result->get($index)->getLocality(), 'state' => $result->get($index)->getAdditionalData()['StateName'], 'zipcode' => $result->get($index)->getPostalCode() ];
+        }
+        return null;
+    }
+
 }
