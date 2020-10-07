ced8077565ff872dd00e1bf89b7dc67e4b606094
Eugene Kataev <cataev.evgeni@yandex.ru>
2020-10-07 08:17:36 +0300

    [ESBL-100] Add try catch Exception in ListingGeoService

--- src/Service/Listing/ListingGeoService.php
+++ src/Service/Listing/ListingGeoService.php
@@ -13,27 +13,35 @@ namespace App\Service\Listing;
 use App\Entity\Listing;
 use App\Service\Geo\GeoCodeService;
 use App\Service\Geo\Point;
+use Psr\Log\LoggerInterface;
 
 class ListingGeoService
 {
     private GeoCodeService $geoCodeService;
     private ListingService $listingService;
+    private LoggerInterface $logger;
 
-    public function __construct(GeoCodeService $geoCodeService, ListingService $listingService)
+    public function __construct(GeoCodeService $geoCodeService, ListingService $listingService, LoggerInterface $logger)
     {
         $this->geoCodeService = $geoCodeService;
         $this->listingService = $listingService;
+        $this->logger = $logger;
     }
 
     public function syncListingCoordinatesFromAddress(Listing $listing)
     {
-        if (is_null($listing->getCoordinates()->getLongitude()) or is_null($listing->getCoordinates()->getLatitude()())) {
-            $listingAddress = $listing->getUnparsedAddress();
-            $listingCoordinates = $this->geoCodeService->getLatLong($listingAddress);
-            if ( is_null($listingCoordinates) ) {
-                throw new \Exception("Coordinates not found for Listing {$listing->getMlsNum()} feed {$listing->getFeedID()}");
+        try {
+            if ( is_null($listing->getCoordinates()->getLongitude()) or is_null($listing->getCoordinates()->getLatitude()()) ) {
+                $listingAddress = $listing->getUnparsedAddress();
+                $listingCoordinates = $this->geoCodeService->getLatLong($listingAddress);
+                if ( is_null($listingCoordinates) ) {
+                    throw new \Exception("Coordinates not found for Listing {$listing->getMlsNum()} feed {$listing->getFeedID()}");
+                }
+                $this->listingService->setListingCoordinates($listing, new Point($listingCoordinates['lat'], $listingCoordinates['lng']));
             }
-            $this->listingService->setListingCoordinates($listing, new Point($listingCoordinates['lat'], $listingCoordinates['lng']));
+        } catch ( \Exception $e ) {
+            $this->logger->error($e->getMessage());
+            $this->logger->error($e->getTraceAsString());
         }
     }
 }
