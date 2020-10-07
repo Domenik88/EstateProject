91efdcb33d316d3cd4da264637801fadd8d43fe8
Eugene Kataev <cataev.evgeni@yandex.ru>
2020-10-07 23:39:27 +0300

    [ESBL-106] Devided autorization for user and admin. Work not complete!

---
+++ src/DataFixtures/AdminFixtures.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by TutMee Co.
+ * User: Domenik88(kataevevgenii@gmail.com)
+ * Date: 07.10.2020
+ *
+ * @package estateblock20
+ */
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Admin;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AdminFixtures extends Fixture
+{
+    private $encoder;
+
+    private $em;
+
+    public function __construct(UserPasswordEncoderInterface $encoder, EntityManagerInterface $entityManager)
+    {
+        $this->encoder = $encoder;
+        $this->em = $entityManager;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $adminsData = [
+            0 => [
+                'username' => 'admin',
+                'role' => ['ROLE_ADMIN'],
+                'password' => 123654
+            ]
+        ];
+
+        foreach ($adminsData as $admin) {
+            $newAdmin = new Admin();
+            $newAdmin->setUsername($admin['username']);
+            $newAdmin->setPassword($this->encoder->encodePassword($newAdmin, $admin['password']));
+            $newAdmin->setRoles($admin['role']);
+            $this->em->persist($newAdmin);
+        }
+
+        $this->em->flush();
+    }
+}
