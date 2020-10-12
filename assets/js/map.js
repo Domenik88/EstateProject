69de9549a525e3ce502050f14f788463897166d3
Eugene Kataev <cataev.evgeni@yandex.ru>
2020-10-13 00:09:13 +0300

    [ESBL-108] Put all active listings on a map. With clustering

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,46 +1,42 @@
     window.initMap = () => {
+        let ajaxPath = $('#map').data('path');
+        let infoWindow = new google.maps.InfoWindow;
         let map = new google.maps.Map(document.getElementById('map'), {
-            center: new google.maps.LatLng(50.8599882, -90.9302304),
+            center: { lat: 50.8599882, lng: -90.9302304 },
             zoom: 4
         });
-        let ajaxPath = $('#map').data('path');
         $.ajax(
             {
                 url: ajaxPath,
                 dataType: "json",
                 method: "POST",
                 success: function (data) {
-                    points(data);
+                    const markers = data.map((markerElem) => {
+                        let infowincontent = document.createElement('div');
+                        let strong = document.createElement('strong');
+                        strong.textContent = markerElem.mlsNum
+                        infowincontent.appendChild(strong);
+                        infowincontent.appendChild(document.createElement('br'));
+                        let text = document.createElement('text');
+                        text.textContent = markerElem.mlsNum
+                        infowincontent.appendChild(text);
+
+                        let marker = new google.maps.Marker({
+                            position:  new google.maps.LatLng(
+                                parseFloat(markerElem.lat),
+                                parseFloat(markerElem.lng)),
+                        });
+                        marker.addListener('click', function () {
+                            infoWindow.setContent(infowincontent);
+                            infoWindow.open(map, marker);
+                        });
+                        return marker;
+                    });
+                    new MarkerClusterer(map, markers, {
+                        imagePath:
+                            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+                    });
                 },
             }
         );
-
-        let infoWindow = new google.maps.InfoWindow;
-        // Change this depending on the name of your PHP or XML file
-        function points(data) {
-            Array.prototype.forEach.call(data, function (markerElem) {
-                var name = markerElem.mlsNum;
-                var address = markerElem.mlsNum;
-                var point = new google.maps.LatLng(
-                    parseFloat(markerElem.lat),
-                    parseFloat(markerElem.lng));
-                var infowincontent = document.createElement('div');
-                var strong = document.createElement('strong');
-                strong.textContent = name
-                infowincontent.appendChild(strong);
-                infowincontent.appendChild(document.createElement('br'));
-
-                var text = document.createElement('text');
-                text.textContent = address
-                infowincontent.appendChild(text);
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: point,
-                });
-                marker.addListener('click', function () {
-                    infoWindow.setContent(infowincontent);
-                    infoWindow.open(map, marker);
-                });
-            });
-        }
     }
