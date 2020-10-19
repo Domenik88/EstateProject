e19cdc564d6f5a5074297ee9e01c0567d9d79503
Eugene Kataev <cataev.evgeni@yandex.ru>
2020-10-19 20:45:13 +0300

    [ESBL-110] Integrate the admin UI

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,27 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin", defaults={"title":"Administrator panel"})
      */
-    public function index()
+    public function index(Request $request)
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'title' =>  $request->attributes->get('title'),
         ]);
     }
 
     /**
-     * @Route("/admin/profile", name="admin_profile")
+     * @Route("/admin/profile", name="admin_profile", defaults={"title":"Administrator panel user profile"})
      */
-    public function profile()
+    public function profile(Request $request)
     {
         return $this->render('admin/profile.html.twig', [
             'controller_name' => 'AdminController',
+            'title' =>  $request->attributes->get('title'),
         ]);
     }
 }
