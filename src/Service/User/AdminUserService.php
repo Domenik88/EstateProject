ebf5797e2ed31be707e65cb710946647416c96d9
Eugene Kataev <cataev.evgeni@yandex.ru>
2020-10-23 23:31:48 +0300

    [ESBL-111] some template changes

---
+++ src/Service/User/AdminUserService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by TutMee Co.
+ * User: Domenik88(kataevevgenii@gmail.com)
+ * Date: 23.10.2020
+ *
+ * @package estateblock20
+ */
+
+namespace App\Service\User;
+
+
+class AdminUserService
+{
+
+}
