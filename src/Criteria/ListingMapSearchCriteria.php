7fc8e0899eb47807d3fbed7de13b509b8656c4fe
Eugene Kataev <cataev.evgeni@yandex.ru>
2021-02-04 12:51:53 +0300

    [ESBL-228] create Search pages (landing)

---
+++ src/Criteria/ListingMapSearchCriteria.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by TutMee Co.
+ * User: Domenik88(kataevevgenii@gmail.com)
+ * Date: 04.02.2021
+ *
+ * @package estateblock20
+ */
+
+namespace App\Criteria;
+
+class ListingMapSearchCriteria
+{
+
+}
